eb7cf855cd1be0934707cea6ba8156140d69726f
amuhaimen <amuhaimenpro829@gmail.com>
2022-09-14 22:34:22 +0600

    new added

---
+++ js/tailwind.config.js
@@ -0,0 +1,25 @@
+tailwind.config = {
+  theme: {
+    extend: {
+      colors: {
+        clifford: "#da373d",
+        red: "red",
+        green: "green",
+      },
+      maxWidth: {
+        container: "1140px",
+      },
+      fontFamily: {
+        pop: "Poppins",
+      },
+      backgroundImage: {
+        banner: "url('images-p/banner.png')",
+        service1: "url('images-p/services1.png')",
+        service2: "url('images-p/services2.png')",
+        service3: "url('images-p/services3.png')",
+        learnmore: "url('images-p/learn-more.png')",
+        blog: "url('images-p/blog.png')",
+      },
+    },
+  },
+};
